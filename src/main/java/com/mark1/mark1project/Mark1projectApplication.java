7afb7ae39094388a6da2702eaf05a195de3bdfc0
nagarjun00595 <arjun00595@gmail.com>
2022-04-25 10:59:51 +0530

    new demo repos

---
+++ src/main/java/com/mark1/mark1project/Mark1projectApplication.java
@@ -0,0 +1,13 @@
+package com.mark1.mark1project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark1projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mark1/mark1project/Mark1projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark1.mark1project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mark1projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
